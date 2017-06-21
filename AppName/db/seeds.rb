29d9afc3e4e4c736b3363243677179368fd17684
Jordan Hudgens <jordan@devcamp.com>
2017-06-21 17:14:24 +0000

    integrated new and create actions for portfolios

--- AppName/db/seeds.rb
+++ AppName/db/seeds.rb
@@ -36,8 +36,8 @@ puts "5 skills created"
         aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
         in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    main_image: "http://placehold.it/600,400",
-    thumb_image: "http:// placehold.it/350x200"
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
     )
 end
  puts "9 portfolio items created"
