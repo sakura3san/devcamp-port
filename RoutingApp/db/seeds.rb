582ab17844d98fbdc974d9b3525cf5789ea1c1ac
Jordan Hudgens <jordan@devcamp.com>
2017-06-28 17:29:17 +0000

    Implemented form configuration for nested attributes

--- RoutingApp/db/seeds.rb
+++ RoutingApp/db/seeds.rb
@@ -1,4 +1,13 @@
- 10.times do |blog|
+3.times do |topic|
+    Topic.create!(
+      title: "Topic #{topic}"
+    )
+    
+end
+ 
+puts "3 Topics created"
+  
+10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}", 
     body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
@@ -12,7 +21,8 @@
     enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit 
     laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum
     iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae
-    consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+    consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+     topic_id: Topic.last.id
 )
 end
 
@@ -26,9 +36,9 @@ puts "10 blog posts created"
 end
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
-    title:"portfolio title: #{portfolio_item}",
+    title:"Portfolio title: #{portfolio_item}",
     subtitle:"Ruby on Rails",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
         eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
@@ -41,3 +51,17 @@ puts "5 skills created"
     )
 end
  puts "9 portfolio items created"
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title:"Portfolio title: #{portfolio_item}",
+    subtitle:"Angular",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+        in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
+    )
+end
