0f97dfd41a1cc9a5e238d14849dee7a4c557de3b
Jordan Hudgens <jordan@devcamp.com>
2017-07-12 16:57:21 +0000

    strategy for building a custom ruby gem

--- AppName/app/controllers/application_controller.rb
+++ AppName/app/controllers/application_controller.rb
@@ -6,7 +6,20 @@
     include DefaultPageContent
 
 
+    before_action :set_copyright
     
-
-  
+    def set_copyright
+      @copyright = DevcampViewTool::Renderer.copyright 'Ashley Bernardi', 'All rights reserved'
+    end
+    
   end
+
+
+  module DevcampViewTool
+    class Renderer
+      def self.copyright name, msg
+        "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+      end
+    end
+  end
+  
