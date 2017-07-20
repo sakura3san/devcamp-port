b768af78b20ab6ab271499d64eebf426d3167d45
Jordan Hudgens <jordan@devcamp.com>
2017-07-20 17:18:22 +0000

    implemented refactor for navigation and active statuses

--- AppName/app/models/portfolio.rb
+++ AppName/app/models/portfolio.rb
@@ -14,7 +14,7 @@
     after_initialize :set_defaults
     
     def set_defaults
-      self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+      self.main_image ||= Placeholder.image_generator(height: '250', width: '250')
       self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
     end
   end
