f40eabb1e2ebf00007c67287ce3d17495c1bf94b
Jordan Hudgens <jordan@devcamp.com>
2017-07-27 16:08:39 +0000

    twitter gem fully styled on the site

--- AppName/app/helpers/application_helper.rb
+++ AppName/app/helpers/application_helper.rb
@@ -47,8 +47,13 @@ module ApplicationHelper
       {
         url: portfolios_path,
         title: 'Portfolio'
-        
-      }
+    
+      },
+      {
+        url: tech_news_path,
+        title: 'Tech News'
+    
+      },
     ]
   end
   def nav_helper style, tag_type
--- AppName/app/helpers/pages_helper.rb
+++ AppName/app/helpers/pages_helper.rb
@@ -1,2 +1,7 @@
 module PagesHelper
+  def twitter_parser tweet:
+  #  tweet = ""
+  #reg expression for twitter gem in order to use it look for it in his show notes
+  
+  end
 end
